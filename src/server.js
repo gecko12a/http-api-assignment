3c753fa9414d6b3c03c4279c257d27641040903c
gecko12a <gecko12a@yahoo.com>
2020-02-07 21:16:29 -0500

    css working?

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
   '/': htmlHandler.getIndex,
-  '/style.css': htmlHandler.getCss,
+  '/style.css': htmlHandler.getCSS,
   '/success': htmlHandler.success,
   '/badRequest': htmlHandler.badRequest,
   '/unauthorized': htmlHandler.unauthorized,
